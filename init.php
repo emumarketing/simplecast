f83911e9e98421d874f24dbf45a071bf021ef2d9
Joshua Rose <joshrose@uoregon.edu>
2013-07-30 11:46:38 -0700

    Implement flash failover on audio player

--- init.php
+++ init.php
@@ -8,11 +8,32 @@ Author: Joshua Rose
 Author URI: http://marketing.uoregon.edu
 */
 
-
+/**
+ * Shortcode handler, renders HTML for player
+ *
+ * @return   String containing audio player HTML.
+ */
 function simplecast_shortcode() {
-	$output = '<audio id="audioplayer" preload="metadata" controls style="width:100%;" >
+	// The basic HTML5 audio component, hopefully this works.
+	$output = '<audio id="simplecast_player" controls>
     			<source src="http://d55-84.uoregon.edu:8000/;stream.nsv&type=mp3" type="audio/mp3">
 			  </audio>';
+
+	// Setup flash player in case we need it
+	$output .= '<script type="text/javascript" src="' . plugins_url('vendor/audio-player/audio-player.js', __FILE__) . '"></script>';
+	$output .= '<script type="text/javascript">  
+            	AudioPlayer.setup("' . plugins_url('vendor/audio-player/player.swf', __FILE__) . '", {  
+                	width: 290  
+            	});
+        		</script>';
+
+    // Check to see if HTML5 player will work, or failover to flash player.    		
+	$output .= '<script type="text/javascript">
+			    var audioTag = document.createElement(\'audio\');
+			    if (!(!!(audioTag.canPlayType) && ("no" != audioTag.canPlayType("audio/mpeg")) && ("" != audioTag.canPlayType("audio/mpeg")))) {
+			        AudioPlayer.embed("simplecast_player", {soundFile: "http://d55-84.uoregon.edu:8000/;stream.nsv&type=mp3"});
+			    }
+				</script>';
 
 	return $output;
 }
